f4013b49e5676a95f3b22d7e291cb2cb451c79a3
Ethan Smoller <ethansmoller@gmail.com>
2019-01-08 14:41:42 -0800

    Removed config from gitignore

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const burger = require("../models/burger.js");
+
+
+const router = function(app) {
+  app.get('/', (req, res) => {
+
+  });
+  app.post('/', (req, res) => {
+
+  });
+  app.put('/', (req, res) => {
+
+  });
+}
--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-
-connection.query('SELECT * FROM `burgers`', (err, results, field) => {
-  console.table(results);
-})
-
+// Listen for client requests
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 })
